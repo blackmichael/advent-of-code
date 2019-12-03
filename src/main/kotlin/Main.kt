80cbccc4d128570d0eafcbc5428ef7ad529e5f6c
Michael.Black2 <Michael.Black2@Target.com>
2019-12-03 09:31:23 -0600

    Day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package main
 fun main() {
     println("day 1")
     day1.main(arrayOf("src/main/resources/day1/input.txt"))
+
+    println("day 2")
+    day2.main(arrayOf("src/main/resources/day2/restored_input.txt"))
 }
